6433d175cf19bd37d5c83e5eb1b0eb33b30caeff
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2023-10-10 16:13:46 +0300

    Add project files.

---
+++ KisiDefteriOnline6/Form1.cs
@@ -0,0 +1,146 @@
+using System.Security.Cryptography;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace KisiDefteriOnline6
+{
+    public partial class Form1 : Form
+    {
+        List<Kisi> kisiler;
+
+        public Form1()
+        {
+            InitializeComponent();
+            VerileriYukle();
+            KisileriListele();
+        }
+
+        private void VerileriYukle()
+        {
+            try
+            {
+                string json = File.ReadAllText("veri.json");
+                kisiler = JsonSerializer.Deserialize<List<Kisi>>(json)!;
+            }
+            catch (Exception)
+            {
+
+                kisiler = OrnekVeriler();
+            }
+        }
+
+        private void KisileriListele()
+        {
+            lstKisiler.Items.Clear();
+
+            foreach (Kisi kisi in kisiler)
+                lstKisiler.Items.Add(kisi);
+        }
+
+        private List<Kisi> OrnekVeriler()
+        {
+            return new List<Kisi>()
+            {
+                new Kisi() { Ad = "Ali", Soyad = "Yýlmaz" },
+                new Kisi() { Ad = "Can", Soyad = "Öztürk" },
+                new Kisi() { Ad = "Cem", Soyad = "Þahin" },
+                new Kisi() { Ad = "Ece", Soyad = "Doðan" }
+            };
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text.Trim();
+            string soyad = txtSoyad.Text.Trim();
+
+            if (ad == "" || soyad == "")
+            {
+                MessageBox.Show("Ad ve soyad zorunlu!");
+                return;
+            }
+
+            Kisi k = new Kisi() { Ad = ad, Soyad = soyad };
+            kisiler.Add(k);
+            KisileriListele();
+            lstKisiler.SelectedItem = k;
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtAd.Focus();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+
+            if (lstKisiler.SelectedItem == null)
+            {
+                MessageBox.Show("Silinmesini istediðiniz kiþiyi seçiniz.");
+                return;
+            }
+            DialogResult dr = MessageBox.Show("Silmek istediðinize emin misinþz ? ", "onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                int sid = lstKisiler.SelectedIndex;
+                Kisi k = (Kisi)lstKisiler.SelectedItem;
+                kisiler.Remove(k);
+                KisileriListele();
+                lstKisiler.SelectedIndex = Math.Min(sid, lstKisiler.Items.Count - 1);
+            }
+
+        }
+
+        private void btnYukari_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid < 1) return;
+            SeciliyiTasi(sid - 1);
+        }
+
+        private void btnAsagi_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid == -1 || sid == lstKisiler.Items.Count - 1) return;
+            SeciliyiTasi(sid + 1);
+        }
+
+        private void SeciliyiTasi(int yeniIndeks)
+        {
+            Kisi k = (Kisi)lstKisiler.SelectedItem!;
+            kisiler.Remove(k);
+            kisiler.Insert(yeniIndeks, k);
+            KisileriListele();
+            lstKisiler.SelectedItem = k;
+        }
+
+        private void btnDuzenle_Click(object sender, EventArgs e)
+        {
+            if (lstKisiler.SelectedItem == null)
+            {
+                MessageBox.Show("Düzenlenmesini istediðiniz kiþiyi seçiniz.");
+                return;
+            }
+
+            Kisi k = (Kisi)lstKisiler.SelectedItem;
+            DuzenleForm frmDuzenle = new DuzenleForm(k);
+            frmDuzenle.ShowDialog();
+            KisileriListele();
+            lstKisiler.SelectedItem = k;
+        }
+
+        private void lstKisiler_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) { btnSil.PerformClick(); }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+            string json = JsonSerializer.Serialize(kisiler,options);
+            File.WriteAllText("veri.json", json);
+        }
+    }
+}
---
+++ KisiDefteriOnline6/Kisi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KisiDefteriOnline6
+{
+    public class Kisi
+    {
+        public string Ad { get; set; } = "";
+
+        public string Soyad { get; set; } = "";
+
+        
+        [JsonIgnore]
+        public string TamAd => $"{Ad} {Soyad}";
+
+        public override string ToString()
+        {
+            return $"Kişi ({Ad} {Soyad})";
+        }
+    }
+}
